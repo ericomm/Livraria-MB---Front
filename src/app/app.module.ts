311baa28907a366a691b7727a46f781f9ef1fe07
Erico Moreira de Miranda <erico.miranda@dbserver.com.br>
2022-03-04 17:25:15 -0300

    componente tela-inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaInicialComponent
   ],
   imports: [
     BrowserModule,

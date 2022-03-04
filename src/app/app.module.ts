5dec4c9fd7f0e59146ddf0b741cd246ed985d10e
erico.miranda <erico.miranda@dbserver.com.br>
2022-03-04 18:06:13 -0300

    refatorando module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
-import { DetalhesComponent } from './detalhes/detalhes.component';
-import { TesteComponent } from './teste/teste.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TelaInicialComponent,
-    DetalhesComponent,
-    TesteComponent
+
   ],
   imports: [
     BrowserModule,

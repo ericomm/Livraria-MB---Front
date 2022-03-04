eb43f57f291852c840d4fe398ced3a0ec44b0109
Erico Moreira de Miranda <erico.miranda@dbserver.com.br>
2022-03-04 17:49:01 -0300

    componente detalhes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+import { DetalhesComponent } from './detalhes/detalhes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TelaInicialComponent
+    TelaInicialComponent,
+    DetalhesComponent
   ],
   imports: [
     BrowserModule,

e1efd0be12401fb1738911bd86838cc429da1f55
erico.miranda <erico.miranda@dbserver.com.br>
2022-03-17 18:06:22 -0300

    tela inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { LivrosComponent } from './tela-inicial/livros/livros.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TelaInicialComponent,
+    CabecalhoComponent,
+    LivrosComponent,
+    RodapeComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
